5833fd3552e2195f84f88e704dec2144fa764ff7
Joseph Pasquale <32305551+joepasquale@users.noreply.github.com>
2020-03-02 15:18:40 -0500

    added to stable match method, changed CSS

--- index.js
+++ index.js
@@ -27,7 +27,9 @@ function addToList(event){
             pref3: inputs[4],
             pref4: inputs[5],
             pref5: inputs[6]
-        }
+        },
+        matched: false,
+        match: null
     };
     if(newMem.brother){
         brothers.push(newMem);
@@ -43,7 +45,45 @@ function matchListHandler(){
     submitButton.addEventListener("click", matchLists);
 }
 
+function matchPair(b, l){
+    b.match = l;
+    l.match = b;
+    b.matched = true;
+    l.matched = true;
+
+}
+
+function unmatchPair(b, l){
+    b.match = null;
+    l.match = null;
+    b.matched = false;
+    l.matched = false;
+}
+
 //uses stable matching to match big and little lists
 function matchLists(event){
-
+    var i = 0;
+    //while current brother can be matched
+    while(!brothers[i].matched){
+        var curBrother = brothers[i];
+        var p;
+        //find prefs
+        for(p in curBrother.prefs){
+            //search little list for name of pref
+            if(littles.includes(curBrother.p)){
+                var l;
+                for(l in littles){
+                    if(curBrother.p == l){
+                        p = l;
+                    }
+                }
+            }
+        }
+        if(!p.matched){
+            matchPair(curBrother, p)
+        }
+        else{
+            
+        }
+    }
 }
